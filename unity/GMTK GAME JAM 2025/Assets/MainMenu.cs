71b4ae6c1229490c3b24fb03ebead2aa614ba163
Space-c2 <50313983+Space-c2@users.noreply.github.com>
2025-07-31 14:43:09 +0700

    Menu

---
+++ unity/GMTK GAME JAM 2025/Assets/MainMenu.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void CloseGame()
+    {
+        Application.Quit();
+    }
+
+
+} 
