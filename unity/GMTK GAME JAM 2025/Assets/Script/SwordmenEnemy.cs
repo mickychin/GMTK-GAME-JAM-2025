f3a635179d3db06a88c6a241868d3e61197fe8f4
mickychin <61143890+mickychin@users.noreply.github.com>
2025-07-31 15:30:05 +0700

    prefabs probably

--- unity/GMTK GAME JAM 2025/Assets/Script/SwordmenEnemy.cs
+++ unity/GMTK GAME JAM 2025/Assets/Script/SwordmenEnemy.cs
@@ -5,13 +5,17 @@ using UnityEngine;
 public class SwordmenEnemy : MonoBehaviour, IDamagable
 {
     [SerializeField] private float maxHealth = 3f;
+    Animator animator;
 
     private float currentHealth;
 
     // Start is called before the first frame update
     void Start()
     {
+        animator = GetComponent<Animator>();
         currentHealth = maxHealth;
+
+        Attack(1);
     }
 
     public void Damage(float damageAmount)
@@ -23,6 +27,15 @@ public class SwordmenEnemy : MonoBehaviour, IDamagable
         {
             //die
             Die();
+        }
+    }
+
+    private void Attack(int AttackMoveSet) //move set start at 1
+    {
+        animator.SetInteger("Attack", AttackMoveSet);
+        if (AttackMoveSet == 1)
+        {
+            //overhead swing
         }
     }
 
