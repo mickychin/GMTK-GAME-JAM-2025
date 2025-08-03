75db7d59094b65def9251d672bb4729dafaef2fb
mickychin <61143890+mickychin@users.noreply.github.com>
2025-08-03 22:51:45 +0700

    ui

---
+++ unity/GMTK GAME JAM 2025/Assets/Script/Setting Menu.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SettingMenu : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+
+    public void SetVolume(float volume)
+    {
+        //Debug.Log(volume);
+        audioMixer.SetFloat("volume", Mathf.Log10(volume) * 20);
+    }
+}
